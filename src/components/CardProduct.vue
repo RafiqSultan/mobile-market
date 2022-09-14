f334082a6fedf2276cf44d572a5ad52307317f41
RafiqSultan <Rafiqaasultan@gmail.com>
2022-09-14 14:50:25 -0700

    FIX card product

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
     <div class="card">
+      <div class="details">
+        <span><i class="fa-solid fa-heart"></i></span>
+        <span><i class="fa-solid fa-heart"></i></span>
+      </div>
+
       <div class="img">
         <img
           src="https://m.media-amazon.com/images/I/61XZQXFQeVL._AC_SL1500_.jpg"
@@ -8,22 +13,18 @@
         />
       </div>
 
-      <div class="bottom-div">
+      <div class="model-Rate">
         <h3>Fanbyn</h3>
-        <div class="top-div">
-          <span><i class="fa-solid fa-heart"></i></span>
+        <div class="star">
+          <i class="fa-solid fa-star" v-for="star in 4"></i>
+          <i class="fa-solid fa-star-half-stroke"></i>
         </div>
       </div>
-      <div class="star">
-        <i class="fa-solid fa-star" v-for="star in 4" :key="satr"></i>
-        <i class="fa-solid fa-star-half-stroke"></i>
-      </div>
 
-      <div class="last-section">
-        <div class="last">
+      <div class="addCard">
+        <div class="addToCart">
           <button><i class="fa-solid fa-plus"></i></button>
         </div>
-
         <p>$<span id="price">200</span></p>
       </div>
     </div>
@@ -31,40 +32,35 @@
 </template>
 
 <style lang="scss" scoped>
+// *Style Card Product
 .card {
   height: 420px;
   width: 250px;
   background-color: #fff;
   border-radius: 10px;
-  padding: 20px;
+  padding: 10px;
   box-sizing: border-box;
   position: relative;
-  font-family: "Poppins", sans-serif;
   overflow: hidden;
   margin-bottom: 100px;
 
   //   Style img
   .img {
     width: 100%;
-    height: 65%;
-    padding: 0.2rem;
+    height: 60%;
+    padding: 0.5rem;
 
     img {
       width: 100%;
       height: 100%;
     }
   }
-  .bottom-div {
-    margin-top: 30px;
+  .details {
     display: flex;
-    justify-content: space-between;
     align-items: center;
-    position: relative;
-
-    .bottom-div h3 {
-      font-weight: 700;
-    }
-    .top-div span {
+    justify-content: space-between;
+    padding: 0 1rem;
+    span {
       height: 30px;
       width: 30px;
       border-radius: 50%;
@@ -77,53 +73,64 @@
       }
     }
   }
-  .star {
-    .fa-star,
-    .fa-star-half-stroke {
-      color: #f0bb0d;
-    }
-  }
-  .last-section {
-    margin-top: 10px;
-    height: 50px;
-    width: 100%;
-    background-color: #132228;
-    border-radius: 10px;
+  .model-Rate {
+    margin-top: 8px;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    transition: all 0.5s;
-    text-align: center;
-    cursor: pointer;
-
-    .last {
-      padding: 0.5rem;
-      button {
-        width: 40px !important;
-        height: 40px;
-        border-radius: 10px;
-        text-align: center;
-        line-height: 20px;
-
-        .fa-plus {
-          font-size: 20px !important;
-        }
+    flex-direction: column;
+    position: relative;
+    padding: 0 0.5rem;
+    h3 {
+      font-weight: 500;
+    }
+    .star {
+      .fa-star,
+      .fa-star-half-stroke {
+        color: #f0bb0d;
       }
     }
+  }
+}
 
-    p {
-      color: #f6f6f6;
-      margin: auto;
-      font-size: 20px;
-    }
+.addCard {
+  margin-top: 10px;
+  height: 50px;
+  width: 100%;
+  background-color: #132228;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  transition: all 0.5s;
+  text-align: center;
+  cursor: pointer;
 
-    &:hover {
-      background-color: black;
-    }
+  .addToCart {
+    padding: 0.5rem;
+    button {
+      width: 40px !important;
+      height: 40px;
+      border-radius: 10px;
+      text-align: center;
 
-    .last button:hover {
-      background-color: #3e3535;
+      .fa-plus {
+        font-size: 25px !important;
+        margin-top: auto;
+      }
     }
+  }
+
+  p {
+    color: #f6f6f6;
+    margin: auto;
+    font-size: 20px;
+  }
+
+  &:hover {
+    background-color: black;
+  }
+
+  .last button:hover {
+    background-color: #3e3535;
   }
 }
 </style> scoped>
--- src/components/store/store.js
+++ src/components/store/store.js
@@ -3,11 +3,60 @@ import { createStore } from 'vuex'
 
 // Create a new store instance.
 const store = createStore({
-  state () {
-    return {
-      count: 0
-    }
-  },
+  state:{
+
+    mobile:[
+        {
+            "id": 1,
+            "model": "Google Pixel 3",
+            "image": "https://the-mobile-store.s3.amazonaws.com/img/phone_1-min.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJBQJFCTCQPALOLKQ%2F20220914%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220914T162254Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=356b8a4214b4bdda2912fb19c7b19a1ccd5d59031a77dddc6b7b3957523ce719",
+            "price": 799
+        },
+        {
+            "id": 2,
+            "model": "Samsung Note 9",
+            "image": "https://the-mobile-store.s3.amazonaws.com/img/phone_2-min.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJBQJFCTCQPALOLKQ%2F20220914%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220914T162254Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=0876b7016d260ec2f2b461635dd7338bbdf1c37b4f99d3ec90248d608e04e0f5",
+            "price": 999
+        },
+        {
+            "id": 3,
+            "model": "Apple iPhone XS",
+            "image": "https://the-mobile-store.s3.amazonaws.com/img/phone_3-min.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJBQJFCTCQPALOLKQ%2F20220914%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220914T162254Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=f5d0ec59751c12f4ec00d98af2bb72f9d4235e8d472656fe940f696edd4fc6a4",
+            "price": 999
+        },
+        {
+            "id": 4,
+            "model": "Samsung Galaxy S9+",
+            "image": "https://the-mobile-store.s3.amazonaws.com/img/phone_4-min.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJBQJFCTCQPALOLKQ%2F20220914%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220914T162254Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=466726a97c8887d6d61c1aa14679e0e4c01d9dd71ecf38a53b047cd2c826bd33",
+            "price": 840
+        },
+        {
+            "id": 5,
+            "model": "Xiaomi Pocophone F1",
+            "image": "https://the-mobile-store.s3.amazonaws.com/img/phone_5-min.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJBQJFCTCQPALOLKQ%2F20220914%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220914T162254Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=4d111fbca08e7f22a30201f1d5335ba14d6354498bf8c2e633e5b81ebf3a0996",
+            "price": 300
+        },
+        {
+            "id": 6,
+            "model": "Xiaomi Mi 8",
+            "image": "https://the-mobile-store.s3.amazonaws.com/img/phone_6-min.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJBQJFCTCQPALOLKQ%2F20220914%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220914T162254Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=43b262c0b6a7903b15180e122252086f573d7402fb949151bf966e53bef56bb6",
+            "price": 400
+        },
+        {
+            "id": 7,
+            "model": "Huawei Mate 10 Pro",
+            "image": "https://the-mobile-store.s3.amazonaws.com/img/phone_7-min.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJBQJFCTCQPALOLKQ%2F20220914%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220914T162254Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=858a82782d5404c57ab54dc34fe689794aea3b2ac25c8bb3495115fc9a48be7b",
+            "price": 800
+        },
+        {
+            "id": 20,
+            "model": "Samsung Galaxy A7 (2018)",
+            "image": "https://the-mobile-store.s3.amazonaws.com/img/samsunggalaxya72018.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJBQJFCTCQPALOLKQ%2F20220914%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20220914T162254Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=e67f6dc45090e0c05c967fde25e7ff8dcb9dbae1b4126e255dea19f733dc76f6",
+            "price": 533
+        }
+    ]
+    
+  }
  
 })
 
