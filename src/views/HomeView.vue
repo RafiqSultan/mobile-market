1f16c86ae0597daa30247c294d22aefe504c909a
RafiqSultan <Rafiqaasultan@gmail.com>
2022-09-15 18:22:54 -0700

    ADD footer component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,18 +3,21 @@
   <TheHeader />
   <LandingScreen />
   <NewProduct />
+  <TheFooter />
 </template>
 
 <script>
 import TheHeader from "../components/Layouts/TheHeader.vue";
 import LandingScreen from "../components/LandingScreen.vue";
 import NewProduct from "../views/NewProduct.vue";
+import TheFooter from "../components/Layouts/TheFooter.vue";
 
 export default {
   components: {
     TheHeader,
     LandingScreen,
     NewProduct,
+    TheFooter,
   },
 };
 </script>
